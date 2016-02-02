2fd98395f7b4326538ec4ecc6f71058b1fce37b2
CraigWells <craigdavidwells@aol.com>
2016-02-02 14:08:13 +0000

    initial commit

---
+++ data.js
@@ -0,0 +1,32 @@
+﻿var data = [
+	{
+		name : "item_1",
+		columnValues : {
+			"value_1" : 200,
+			"value_2" : 206,
+			"value_3" : 207,
+			"value_4" : 300,
+			"value_5" : 303,
+			"value_6" : 280,
+			"value_7" : 278,
+			"value_8" : 290
+		}
+	},
+	{
+		name : "item_2",
+		columnValues : {
+			"value_1" : 186,
+			"value_2" : 199,
+			"value_3" : 201,
+			"value_4" : 222,
+			"value_5" : 250,
+			"value_6" : 234,
+			"value_7" : 236,
+			"value_8" : 240,
+			"value_9" : 210,
+			"value_10" : 220,
+			"value_11" : 230,
+			"value_12" : 244
+		}
+	}	
+];
---
+++ logic.js
@@ -0,0 +1,76 @@
+﻿var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+
+function addCanvasToDom(){
+	var canvasInstance = "<canvas id=\""+canvas_id+"\" width=\"300\" height=\"150\>Your browser does not support the HTML5 canvas tag.</canvas>";
+}
+
+function valuesToArrays(){
+	var len = data.length;
+	for(var i=0; i<len;i++){
+		data[i].values = [];
+		for(key in data[i].columnValues){
+			data[i].values.push(data[i].columnValues[key]);
+		}
+	}
+};
+
+function calcDifValues(){
+	var len = data.length;
+	for(var i=0; i < len; i++){
+		calcDif(i);
+	}
+};	
+
+function calcDif(count){
+	var values = data[count].values;
+	data[count].difs = [];
+	var len = values.length;
+	for(var i=0; i<len;i++){
+		if(i != (len-1)){
+			var thisDif = (values[i+1]-values[i]);
+			data[count].difs.push(thisDif);
+		}
+	}
+};
+
+function plotPoints(){
+	var range = data[0].difs.length;
+	var spacesX = Math.floor(c.clientWidth / range);
+	data[0].pointsX = [];
+	var count = 0;
+	for(var i = 0; i < range; i++){
+		count += spacesX;
+		data[0].pointsX.push(count);
+	}
+};
+
+function drawGraph(){
+	drawMedian();
+	plotPoints();
+};
+
+function drawMedian(){
+	var colour = "#909090"; 
+	var ypos = c.clientHeight/2;
+	drawLine(0, ypos, 300, ypos, colour);
+};
+
+function drawLine(a, b, c, d, colour){
+	ctx.beginPath();
+	ctx.moveTo(a, b);
+	ctx.lineTo(c, d);
+	if(colour){ctx.strokeStyle = colour;}else{ctx.strokeStyle = "#000000"};
+	ctx.stroke();
+};
+
+function init(){
+	addCanvasToDom();
+	valuesToArrays();
+	calcDifValues(0);
+	drawGraph();
+	console.log(data);
+};
+
+
+init();
